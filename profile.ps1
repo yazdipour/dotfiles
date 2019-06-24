35d27fe1341b2afc9a8b17965c11bc4cf41b9f41
Shahriar <shahriar.yazdipour@outlook.com>
2019-06-24 10:04:03 +0430

    Update with Comments

--- profile.ps1
+++ profile.ps1
@@ -1,31 +1,58 @@
+# SETUP ENV
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 #New-Item $profile -Type File -Force
 #notepad $profile
-#function AsAdminOrNot {
-#	$wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-#	$prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
-#	$adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-#	$prp.IsInRole($adm)
-#}
 
+# INIT
+#Start-Transcript 
+
+# THEME
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Agnoster
 
+# ENV
+$repo= Join-Path $env:USERPROFILE -ChildPath \source\repos
+$dl= Join-Path $env:USERPROFILE -ChildPath \Downloads
+$docs= Join-Path $env:USERPROFILE -ChildPath \Documents
+
+# UTILS
+function clean-bin{Get-ChildItem .\ -include bin,obj -Recurse | ForEach-Object ($_) { remove-item $_.fullname -Force -Recurse }}
+Set-Alias bget Start-BitsTransfer
+
+# SUDO
+Set-Alias su run-as-admin
+function run-as-admin{Start-Process powershell -Verb runas}
+function AsAdminOrNot {
+	$wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+	$prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
+	$adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+	$prp.IsInRole($adm)
+}
+
+# WIFI-SHARE
 function turn-on-hotspot { netsh wla start ho}
 function turn-off-hotspot { netsh wla stop ho}
-function p1 {ping 1.1.1.1 -t}
-function p192 {ping 192.168.1.1 -t}
+Set-Alias w turn-on-hotspot
+Set-Alias w! turn-off-hotspot
+
+# ANDROID
 function adblog($_) {ADB logcat $_}
 function adbloge($_) {ADB logcat *:E}
 function adbconnect($_) {ADB connect $_}
 function adbemulator($_) {cd $env:android_home/tools/ ; emulator.exe -avd $_}
-function run-as-admin{Start-Process powershell -Verb runas}
-function clean-bin{Get-ChildItem .\ -include bin,obj -Recurse | ForEach-Object ($_) { remove-item $_.fullname -Force -Recurse }}
-function update-fork($_){git remote add upstream $_ ; git fetch upstream ; git pull upstream master}
+
+# PING
+function p1 {ping 1.1.1.1 -t}
+function p192 {ping 192.168.1.1 -t}
+
+# profile
 function updatep {. $profile}
-function editp {nano $profile}
-function cr($_){code $_ -r}
+function editp {notepad $profile}
+
+
+# GIT
+Set-Alias g git
 function gitm($_){git commit -m $_}
 function gitc($_){git clone $_}
 function gita($_){git add $_}
@@ -34,20 +61,22 @@ function gitd($_){git diff $_}
 function git-noupdate($_){git update-index --assume-unchanged $_}
 function gitrm($_){git rm --cached $_}
 function gits(){git status}
-#######
-Set-Alias w turn-on-hotspot
-Set-Alias w! turn-off-hotspot
-Set-Alias c code
-Set-Alias e explorer
-Set-Alias su run-as-admin
-Set-Alias g git
-Set-Alias l ls
-Set-Alias w3m elinks
-Set-Alias bget Start-BitsTransfer
-$repo= Join-Path $env:USERPROFILE -ChildPath \Documents\Repos\_MINE\
-$dl= Join-Path $env:USERPROFILE -ChildPath \Downloads\
+function update-fork($_){git remote add upstream $_ ; git fetch upstream ; git pull upstream master}
 
-#######
-#if (AsAdminOrNot) {Write-Output "~Admin~Be careful!"}
-#else {Write-Output "~NotAdmin~"}
-#Start-Transcript
+# Search in files
+function find($file, $txt){Get-ChildItem -Recurse -Include $file | select-string $txt}
+
+# VSCODE
+Set-Alias c code
+function cr($_){code $_ -r}
+
+# ALIAS
+Set-Alias l ls
+Set-Alias e explorer
+Set-Alias w3m elinks
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
