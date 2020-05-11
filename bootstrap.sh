34e652b391a0273965f514c7279ebd5280f41d3b
Shahriar <shahriar.yazdipour@outlook.com>
2020-05-12 01:07:25 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo -s
-apt install curl tmux htop zsh neofetch python3
+apt install curl tmux htop zsh neofetch python3 hexcurse bat -y
 # ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # ssh
-apt install openssh-server
+sudo apt install openssh-server -y
 sudo service ssh status
 sudo ufw allow ssh
 sudo ufw enable
@@ -16,7 +16,7 @@ sudo ufw enable
 # To install nvim https://github.com/neovim/neovim/wiki/Installing-Neovim#ubuntu
 sudo apt-get install software-properties-common
 sudo apt update
-sudo apt install neovim python3-neovim python3-dev python3-pip
+sudo apt install neovim python3-neovim python3-dev python3-pip -y
 # vimplug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # to create config file for neovim
