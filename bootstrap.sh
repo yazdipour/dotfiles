b85ae60871e4436803e727dd3831466358e4c74f
Shahriar <shahriar.yazdipour@outlook.com>
2020-05-11 16:45:49 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
-# To install nvim https://github.com/neovim/neovim/wiki/Installing-Neovim#ubuntu
+#!/bin/bash
 sudo -s
-apt install curl tmux htop zsh neofetch python3 neovim python-neovim python3-neovim
+apt install curl tmux htop zsh neofetch python3
 # ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-apt install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # ssh
 apt install openssh-server
@@ -12,11 +12,16 @@ sudo service ssh status
 sudo ufw allow ssh
 sudo ufw enable
 
+# nvim
+# To install nvim https://github.com/neovim/neovim/wiki/Installing-Neovim#ubuntu
+sudo apt-get install software-properties-common
+sudo apt update
+sudo apt install neovim python3-neovim python3-dev python3-pip
 # vimplug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # to create config file for neovim
 mkdir ~/.config; mkdir ~/.config/nvim; touch ~/.config/nvim/init.vim 
-pip install jedi #auto-comp plugin - options of vim https://github.com/deoplete-plugins/deoplete-jedi
+pip3 install jedi #auto-comp plugin - options of vim https://github.com/deoplete-plugins/deoplete-jedi
 
 # nvim instead of vi
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
