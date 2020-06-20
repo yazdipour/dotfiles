01068a81c311774cf7197b728bfd0b22adc7d43b
Shahriar <shahriar.yazdipour@outlook.com>
2020-06-20 17:53:02 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo -s
-apt install curl htop zsh neofetch python3 hexcurse figlet bat aria2 golang-go -y
+apt install curl htop zsh neofetch python3 dos2unix hexcurse figlet bat aria2 golang-go -y
 
 # tmux
 sudo apt install tmux
-sudo touch ~/.tmux.conf #copy configs here and then kill server
+wget https://raw.githubusercontent.com/yazdipour/dot-files/master/.tmux.config -O ~/.tmux.conf #copy configs here and then kill server
 tmux kill-server
 
 # ssh
@@ -22,6 +22,7 @@ sudo apt install neovim python3-neovim python3-dev python3-pip -y
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # to create config file for neovim
 mkdir ~/.config; mkdir ~/.config/nvim; touch ~/.config/nvim/init.vim 
+wget https://raw.githubusercontent.com/yazdipour/dot-files/master/init.vim -O ~/.config/nvim/init.vim
 pip3 install jedi #auto-comp plugin - options of vim https://github.com/deoplete-plugins/deoplete-jedi
 
 # nvim instead of vi
@@ -35,12 +36,17 @@ sudo update-alternatives --config editor
 # bashtop
 sudo add-apt-repository ppa:bashtop-monitor/bashtop
 sudo apt update
-sudo apt install bashtop
+sudo apt install bashtop -y
 
 # w3m
-sudo apt-get install w3m w3m-img
+sudo apt-get install w3m w3m-img -y
 
 # ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp ~/.zshrc  ~/.zshrc_bk
+wget https://raw.githubusercontent.com/yazdipour/dot-files/master/.zshrc -O ~/.zshrc
+wget https://raw.githubusercontent.com/yazdipour/dot-files/master/dir_colors_lightblue -O $HOME/.dir_colors
+dos2unix ~/.zshrc
+dos2unix $HOME/.dir_colors
