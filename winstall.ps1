dd6c7cda2b0b13bcce85415e93efbf6041ce7ada
Shahriar <shahriar.yazdipour@outlook.com>
2022-04-06 10:52:05 +0200

    Update winstall.ps1

--- winstall.ps1
+++ winstall.ps1
@@ -56,6 +56,7 @@ scoop install terraform
 scoop install nano
 scoop install espanso
 scoop bucket add nerd-fonts
+scoop install firacode
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
