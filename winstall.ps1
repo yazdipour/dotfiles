86ecf6259401705e0caf177e62516ca7d36918ba
Shahriar <shahriar.yazdipour@outlook.com>
2022-04-29 12:41:37 +0200

    Update winstall.ps1

--- winstall.ps1
+++ winstall.ps1
@@ -68,3 +68,4 @@ scoop install terminal-icons
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install kubernetes-helm -y
+choco install winmtr-redux
