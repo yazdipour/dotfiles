5e4748d94ede30f96e71304931301d01f9f4d99c
Shahriar <sh.yazdipour@hotmail.com>
2022-09-04 14:53:05 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,31 +3,30 @@
 #New-Item $profile -Type File -Force
 #notepad $profile
 
-Set-Alias cat bat -Option AllScope
-Set-Alias grep findstr
 Set-Alias l ls
 Set-Alias e explorer
-Set-Alias w3m elinks
+Set-Alias v nvim
+Set-Alias cat bat -Option AllScope
+Set-Alias grep findstr
+Set-Alias bget Start-BitsTransfer
+Set-Alias rmbin Clear-RecycleBin
+function pt{procs --tree}
+function pw{procs --watch}
 function su{Start-Process powershell -Verb runas}
-function wsls{wsl --shutdown}
-function wsll{wsl -l -v}
 function find($file, $txt){Get-ChildItem -Recurse -Include $file | select-string $txt} # Search in files
 function openbin{start shell:RecycleBinFolder}
-Set-Alias rmbin Clear-RecycleBin
 function rmbuildfolders{Get-ChildItem .\ -include bin,obj,target -Recurse | ForEach-Object ($_) { remove-item $_.fullname -Force -Recurse }}
 function smi{Watch-Command -Cont -ClearScreen {nvidia-smi.exe}} #while (1){cls;nvidia-smi;sleep 1}
+function wifi{netsh wlan show networks mode=Bssid}
 
 # ENV
 $repo= Join-Path $env:USERPROFILE -ChildPath \Source
 $dl= Join-Path $env:USERPROFILE -ChildPath \Downloads
 $docs= Join-Path $env:USERPROFILE -ChildPath \Documents
 
-# WIFI-SHARE
-function turn-on-hotspot { netsh wla start ho}
-function turn-off-hotspot { netsh wla stop ho}
-Set-Alias w turn-on-hotspot
-Set-Alias w! turn-off-hotspot
-function wifi{netsh wlan show networks mode=Bssid}
+#WSL
+function wsls{wsl --shutdown}
+function wsll{wsl -l -v}
 
 # ANDROID
 function adblog($_) {ADB logcat $_}
@@ -41,7 +40,7 @@ function p192 {ping 192.168.1.1 -t}
 
 # profile
 function updatep {. $profile}
-function editp {notepad $profile}
+function editp {v $profile}
 
 # GIT
 Set-Alias g git
@@ -65,7 +64,6 @@ function beep(){[console]::beep(500,300)}
 function beeep(){[console]::beep(2000,500)}
 
 ## UTILS
-Set-Alias bget Start-BitsTransfer
 function weather($_){(curl "http://wttr.in/${_}").Content}
 function mp4tomp3(){ffmpeg -i}
 function compressmp4($_){ffmpeg -i $_ -vcodec h264 -acodec mp2 output.mp4}
