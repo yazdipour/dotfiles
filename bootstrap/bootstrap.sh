3062ac5b5d7d608965a71984bf1b3dbca55898dc
Shahriar <yazdipour@users.noreply.github.com>
2023-12-17 01:13:21 +0100

    Update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -15,32 +15,20 @@ sudo apt install fzf -y
 
 cargo install --locked procs bat eza fd-find zoxide
 
-# setup dot-files
-git clone --depth 1 https://github.com/yazdipour/dot-files
-ln -s ~/dot-files/zsh/.zshrc  ~/.zshrc
-ln -s ~/dot-files/tmux/.tmux.conf ~/.tmux.conf
-
-# tmux
-sudo apt install tmux -y
-wget https://raw.githubusercontent.com/yazdipour/dot-files/master/.tmux.config -O ~/.tmux.conf #copy configs here and then kill server
-git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux kill-server
-
 # ssh
 sudo apt install openssh-server -y
 sudo service ssh status
 sudo ufw allow ssh
 sudo ufw enable
 
+# tmux
+sudo apt install tmux -y
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux kill-server
+
 #zsh
 sudo apt install zsh -y
 curl -L git.io/antigen > $HOME/antigen.zsh
-wget https://raw.githubusercontent.com/yazdipour/dot-files/master/.zshrc -O ~/.zshrc
-dos2unix ~/.zshrc
-
-# Copilot
-npm install -g @githubnext/github-copilot-cli
-github-copilot-cli auth
 
 #############
 #### VIM ####
@@ -59,14 +47,14 @@ sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
 
-###############
-#### More? ####
-###############
-pip install yt-dlp
-
 # Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install node
 
 # font
 curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/CascadiaCode.zip
+
+# setup dotfiles
+# ln -s ~/dotfiles/zsh/.zshrc  ~/.zshrc
+# ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+#dos2unix ~/.zshrc
