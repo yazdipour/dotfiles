c3987bb52a69da58e0653f19a726e607668a61b1
Shahriar Yazdipour <yazdipour@users.noreply.github.com>
2024-04-24 11:31:40 +0200

    treesitter update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return { -- Highlight, edit, and navigate code
       'html',
       'lua',
       'luadoc',
-      'markdown',
+      'markdown_inline',
       'vim',
       'vimdoc',
       'java',
