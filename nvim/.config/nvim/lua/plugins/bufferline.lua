bc9fe9f08689ded593ef0cc75f6d560769e6b6f3
Shahriar Yazdipour <8194807+yazdipour@users.noreply.github.com>
2025-02-22 11:35:57 +0100

    feat: add bufferline.nvim plugin configuration with keybindings for navigation

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers", -- set to "tabs" to only show tabpages instead
+          numbers = "none",
+          close_command = "bdelete! %d",
+          indicator = {
+            icon = '▎', -- this should be omitted if indicator style is not 'icon'
+            style = 'icon',
+          },
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end,
+          separator_style = "thin",
+          always_show_bufferline = true,
+        },
+      })
+
+      -- Keymaps for buffer navigation
+      vim.keymap.set('n', '<leader>bp', ':BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+      vim.keymap.set('n', '<leader>bn', ':BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+      vim.keymap.set('n', '<leader>bc', ':BufferLinePickClose<CR>', { desc = 'Close buffer' })
+      vim.keymap.set('n', '<leader>bb', ':BufferLinePick<CR>', { desc = 'Pick buffer' })
+    end,
+  }
+}
