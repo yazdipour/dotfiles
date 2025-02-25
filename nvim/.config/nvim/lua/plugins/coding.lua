24df3e7cafa524cf402bc0db8415a3da42624531
Shahriar Yazdipour <yazdipour@users.noreply.github.com>
2025-02-25 17:09:31 +0100

    feat: add incremental renaming plugin and log highlighting support

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,10 +1,17 @@
 return {
-  'github/copilot.vim',
-  'mg979/vim-visual-multi', -- Multiple cursors
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment visual regions/lines
+  { "fei6409/log-highlight.nvim", event = "BufRead *.log", opts = {} },
+  'mg979/vim-visual-multi',               -- Multiple cursors
+  'tpope/vim-sleuth',                     -- Detect tabstop and shiftwidth automatically
+  { 'numToStr/Comment.nvim',      opts = {} }, -- "gc" to comment visual regions/lines
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'folke/todo-comments.nvim',   event = 'VimEnter',      dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   require('kickstart.plugins.autopairs'),
   require('kickstart.plugins.indent_line'),
+  { -- Plugin for incremental renaming to not rename all at once
+    'smjonas/inc-rename.nvim',
+    config = function()
+      require('inc_rename').setup()
+    end,
+    vim.keymap.set("n", "<leader>cr", ":IncRename ", { desc = "Incremental [R]ename" })
+  }
 }
--- nvim/.config/nvim/lua/plugins/inc-rename.lua
+++
@@ -1,7 +0,0 @@
-return { -- Plugin for incremental renaming to not rename all at once
-  'smjonas/inc-rename.nvim',
-  config = function()
-    require('inc_rename').setup()
-  end,
-  vim.keymap.set("n", "<leader>cr", ":IncRename ", { desc = "Incremental [R]ename" } )
-}
