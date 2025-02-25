db8ba1117dc4c1d23f756180e71609b98e7ba63f
Shahriar Yazdipour <yazdipour@users.noreply.github.com>
2025-02-25 17:09:18 +0100

    feat: configure mini.surround with custom key mappings for enhanced functionality

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -16,7 +16,19 @@ return { -- Collection of various small independent plugins/modules
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
+        require('mini.surround').setup({
+            opts = {
+                mappings = {
+                    add = "<leader>ma", -- Add surrounding in Normal and Visual modes
+                    delete = "<leader>md", -- Delete surrounding
+                    find = "<leader>mf", -- Find surrounding (to the right)
+                    find_left = "<leader>mF", -- Find surrounding (to the left)
+                    highlight = "<leader>mh", -- Highlight surrounding
+                    replace = "<leader>mr", -- Replace surrounding
+                    update_n_lines = "<leader>mn", -- Update `n_lines`
+                }
+            }
+        })
 
         -- Simple and easy statusline.
         --  You could remove this setup call if you don't like it,
