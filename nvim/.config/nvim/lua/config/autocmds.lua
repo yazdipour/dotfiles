f2b5f44f01292ad1327f975fa3a33f9cbec3be29
Shahriar Yazdipour <8194807+yazdipour@users.noreply.github.com>
2025-02-27 01:33:36 +0100

    feat(nvim): add autocmd to change directory to current file's directory

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto change directory to the current file's directory
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  desc = "Set the current working directory to the directory of the current file",
+  callback = function()
+    -- Only change directory if the current buffer is a normal file
+    if vim.bo.buftype == "" and vim.fn.expand("%:p") ~= "" then
+      vim.cmd("lcd %:p:h")
+    end
+  end,
+})
