5f0b99ffca66fef804913daffa53107dbf7d9bc2
Shahriar Yazdipour <yazdipour@users.noreply.github.com>
2025-03-06 04:56:18 +0100

    feat: more nvim plugins

---
+++ nvim/.config/nvim/lua/plugins/devcontainer.lua
@@ -0,0 +1,13 @@
+return {
+  "https://codeberg.org/esensar/nvim-dev-container",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+}
+-- DevcontainerStart - start whatever is defined in devcontainer.json
+-- DevcontainerAttach - attach to whatever is defined in devcontainer.json
+-- DevcontainerExec - execute a single command on container defined in devcontainer.json
+-- DevcontainerStop - stop whatever was started based on devcontainer.json
+-- DevcontainerStopAll - stop everything started with this plugin (in current session)
+-- DevcontainerRemoveAll - remove everything started with this plugin (in current session)
+-- DevcontainerLogs - open plugin log file
+-- DevcontainerEditNearestConfig - opens nearest devcontainer.json file if it exists, or creates a new one if it does not
+-- Check out :h devcontainer for full list of functions.
